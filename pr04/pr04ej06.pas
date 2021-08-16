0f545d971e41c8038955e21ccc998ded113aff6b
quelopelo <88778736+quelopelo@users.noreply.github.com>
2021-08-16 10:47:24 -0300

    Add files via upload

---
+++ pr04/pr04ej06.pas
@@ -0,0 +1,29 @@
+program pr04ej06;
+
+var
+n, i, num, min, max : integer;
+
+begin
+
+write('Ingrese un valor para n (n > 1): ');
+readln(n);
+
+writeln('Ingrese ', n, ' enteros: ');
+
+readln(num);
+min := num;
+max := num;
+
+for i := 1 to n-1 do
+	begin
+	readln(num);
+	if num < min then
+		min := num;
+	if num > max then
+		max := num
+	end;
+ 
+writeln('El mayor entero ingresado es: ', max);
+writeln('El menor entero ingresado es: ', min)
+
+end.
