444b76c368b766f80d91eb96fbdaa1e02c99059c
quelopelo <88778736+quelopelo@users.noreply.github.com>
2021-08-17 16:41:38 -0300

    Update pr04ej05.pas

--- pr04/pr04ej05.pas
+++ pr04/pr04ej05.pas
@@ -10,12 +10,12 @@ readln(n);
 
 write('Los divisores son: 1'); 	{1 siempre es divisor}
 
-for i := 2 to (n div 2) do		{Se itera hasta la mitad por eficiencia}
-	if n mod i = 0 then
-		write(' ', i:1);
+for i := 2 to (n div 2) do	{Se itera hasta la mitad por eficiencia}
+	if	n mod i = 0
+	then	write(' ', i:1);
 		
-if n > 1 then 					{Si n = 1, el divisor 1 ya se ingresó}
-	write(' ', n:1); 				{n siempre es divisor}
+if	n > 1			{Si n = 1, el divisor 1 ya se ingresó}
+then	write(' ', n:1); 	{n siempre es divisor}
 
 writeln
 
