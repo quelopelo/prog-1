dacb358deabb1fa1fa73cd53fd88ec2db47ce258
quelopelo <88778736+quelopelo@users.noreply.github.com>
2021-08-17 16:43:28 -0300

    Update pr04ej06.pas

--- pr04/pr04ej06.pas
+++ pr04/pr04ej06.pas
@@ -14,14 +14,14 @@ read(num);
 min := num;
 max := num;
 
-for i := 2 to n do
-	begin
+for	i := 2 to n do
+begin
 	read(num);
-	if num < min then
-		min := num;
-	if num > max then
-		max := num
-	end;
+	if	num < min
+	then	min := num;
+	if	num > max
+	then	max := num
+end;
  
 writeln('El mayor entero ingresado es: ', max:1);
 writeln('El menor entero ingresado es: ', min:1)
