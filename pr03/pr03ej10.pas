35dd8c02a56bf1e873503cc7248280749a7e1c3d
quelopelo <88778736+quelopelo@users.noreply.github.com>
2021-08-17 16:33:31 -0300

    Update pr03ej10.pas

--- pr03/pr03ej10.pas
+++ pr03/pr03ej10.pas
@@ -1,18 +1,17 @@
 program p03ej10;
 
-var
-num: integer;
+var	num: integer;
 
 procedure tres_cifras (num: integer);
 var aux: integer;
 begin
-aux := num div 100;
-write(aux:1);
-num := num - aux * 100;
-aux := num div 10;
-write(aux:1);
-num := num - aux * 10;
-write(num:1);
+	aux := num div 100;
+	write(aux:1);
+	num := num - aux * 100;
+	aux := num div 10;
+	write(aux:1);
+	num := num - aux * 10;
+	write(num:1);
 end;
 
 begin
@@ -21,13 +20,13 @@ write ('Ingrese un número entero de hasta cuatro cifras: ');
 readln (num);
 
 write ('El número con separador de miles es: ');
-if num < 1000
-then write (num:1)
+if	num < 1000
+then	write (num:1)
 else
-	begin
+begin
 	write ((num div 1000):1, '.');
 	tres_cifras (num mod 1000)
-	end;
+end;
 writeln
 
 end.
