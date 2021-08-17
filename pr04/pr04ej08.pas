66db288fe679259af404fbfc2c70a58f56773472
quelopelo <88778736+quelopelo@users.noreply.github.com>
2021-08-17 16:44:22 -0300

    Update pr04ej08.pas

--- pr04/pr04ej08.pas
+++ pr04/pr04ej08.pas
@@ -12,11 +12,11 @@ write('Ingrese un valor (natural) para n: ');
 readln(n);
 
 (*Salida de datos*)
-for i := 1 to n do
-	begin
-	for j := 1 to n-i+1 do
+for	i := 1 to n do
+begin
+	for	j := 1 to n-i+1 do
 		write(c);
 	writeln;
-	end
+end
 
 end.
