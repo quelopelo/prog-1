4f531477f927dab24bac1cf07f7ecf37bea27dad
quelopelo <88778736+quelopelo@users.noreply.github.com>
2021-08-17 16:45:13 -0300

    Update pr04ej10.pas

--- pr04/pr04ej10.pas
+++ pr04/pr04ej10.pas
@@ -1,8 +1,7 @@
 program pr04ej10;
 
-var
-n, i : integer;		{4 bytes: -2147483648 a 2147483647}
-factorial : int64;	{8 bytes: -9223372036854775808 a 9223372036854775807}
+var	n, i : integer;		{4 bytes: -2147483648 a 2147483647}
+	factorial : int64;	{8 bytes: -9223372036854775808 a 9223372036854775807}
 
 begin
 
@@ -12,7 +11,7 @@ readln(n);
 
 (*Cálculo del factorial*)
 factorial := 1;
-for i := 2 to n do
+for	i := 2 to n do
 	factorial := factorial * i;
 
 (*Salida de datos*)
