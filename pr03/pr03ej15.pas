07b3e4486e4ba475d78cd7135036de057a84fa7a
quelopelo <88778736+quelopelo@users.noreply.github.com>
2021-08-17 16:38:58 -0300

    Update pr03ej15.pas

--- pr03/pr03ej15.pas
+++ pr03/pr03ej15.pas
@@ -5,26 +5,26 @@ var	c1, c2, c3: char;
 
 (*Procedimiento que convierte un solo dígito*)
 procedure convertir (c: char; var n: integer);
-	begin
+begin
 	case c of
-	'0':		n := 0;
-	'1':		n := 1;
-	'2':		n := 2;
-	'3':		n := 3;
-	'4':		n := 4;
-	'5':		n := 5;
-	'6':		n := 6;
-	'7':		n := 7;
-	'8':		n := 8;
-	'9':		n := 9;
-	'A', 'a':	n := 10;
-	'B', 'b':	n := 11;
-	'C', 'c':	n := 12;
-	'D', 'd':	n := 13;
-	'E', 'e':	n := 14;
-	'F', 'f':	n := 15
+		'0':		n := 0;
+		'1':		n := 1;
+		'2':		n := 2;
+		'3':		n := 3;
+		'4':		n := 4;
+		'5':		n := 5;
+		'6':		n := 6;
+		'7':		n := 7;
+		'8':		n := 8;
+		'9':		n := 9;
+		'A', 'a':	n := 10;
+		'B', 'b':	n := 11;
+		'C', 'c':	n := 12;
+		'D', 'd':	n := 13;
+		'E', 'e':	n := 14;
+		'F', 'f':	n := 15
 	end
-	end;
+end;
 
 begin
 
