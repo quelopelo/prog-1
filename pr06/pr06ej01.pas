845007cbcc0ff23bc7d2b3dbd8f2f7706f5c21ae
quelopelo <88778736+quelopelo@users.noreply.github.com>
2021-08-26 11:55:10 -0300

    Add files via upload

--- pr06/pr06ej01.pas
+++ pr06/pr06ej01.pas
@@ -1,6 +1,6 @@
 program pr06ej01;
 
-var	m, n, mcd : integer;
+var	m, n, aux : integer;
 
 begin
 
@@ -15,9 +15,9 @@ readln(n);
 if	m > n
 then
 begin
-	mcd := m;
+	aux := m;
 	m := n;
-	n := mcd
+	n := aux
 end;
 
 (*Cálculo del máximo común divisor y salida de datos*)
@@ -25,21 +25,13 @@ if	n = 0
 then	writeln('Error: m y n no pueden ser los dos 0')
 else
 begin
-	if	m = 0
-	then	mcd := n		{MCD (m,0) = m}
-	else
+	while	m > 0 do		{MCD (n,0) = n}	
 	begin
-		n := n mod m;		{MCD (n,m) = MCD (m, n mod m)}
-		if	n = 0
-		then	mcd := m	{MCD (m,0) = m}	
-		else
-		begin
-			mcd := n;	{n < m (resto de la división}
-			while	(m mod mcd <> 0) or (n mod mcd <> 0) do
-				mcd := mcd - 1
-		end
+		aux := n mod m;		{MCD (n,m) = MCD (m, n mod m)}
+		n := m;
+		m := aux;		{n < m (resto de la división}
 	end;
-	writeln('MCD = ', mcd:1)	{Salida de datos}
-end;
+	writeln('MCD = ', n:1)		{Salida de datos}
+end
 
 end.
