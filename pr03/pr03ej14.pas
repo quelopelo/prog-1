230e7fa138e161817d55b3e16783c5d4452e2666
quelopelo <88778736+quelopelo@users.noreply.github.com>
2021-08-27 12:55:48 -0300

    Update pr03ej14.pas

--- pr03/pr03ej14.pas
+++ pr03/pr03ej14.pas
@@ -1,7 +1,7 @@
 program pr03ej14;
 
 var
-a, b, c, det: real;
+a, b, c, dis: real;
 
 begin
 
@@ -11,26 +11,26 @@ writeln ('ax² + bx + c = 0');
 write ('Ingrese los valores de a, b y c separados por espacios: ');
 readln (a, b, c);
 
-(*Cálculo del determinante*)
-det := b * b - 4 * a * c;
+(*Cálculo del discriminante*)
+dis := b * b - 4 * a * c;
 
 (*Salida de datos*)
-if	det > 0
+if	dis > 0
 then 
 begin
-	det := sqrt(det);
+	dis := sqrt(dis);
 	write ('Dos raíces reales diferentes: ');
-	write (((-b - det) / (2 * a)):6:2, ' y ');
-	writeln (((-b + det) / (2 * a)):6:2)
+	write (((-b - dis) / (2 * a)):6:2, ' y ');
+	writeln (((-b + dis) / (2 * a)):6:2)
 end
 else
-	if	det = 0
+	if	dis = 0
 	then	writeln ('Dos raíces reales iguales: ', (-b / (2 * a)):6:2)
 	else
 	begin
-		det := sqrt(-det);
+		dis := sqrt(-dis);
 		write ('Dos raíces complejas diferentes: ');
 		write ((-b / (2 * a)):6:2, ' (+/-) i ');
-		writeln ((det / (2 * a)):6:2)
+		writeln ((dis / (2 * a)):6:2)
 	end
 end.
