5bcb9df554da4a518aac0041dbf676cf333669eb
quelopelo <88778736+quelopelo@users.noreply.github.com>
2021-08-30 11:24:28 -0300

    Update pr03ej14.pas

--- pr03/pr03ej14.pas
+++ pr03/pr03ej14.pas
@@ -15,17 +15,18 @@ readln (a, b, c);
 dis := b * b - 4 * a * c;
 
 (*Salida de datos*)
-if	dis > 0
-then 
-begin
-	dis := sqrt(dis);
-	write ('Dos raíces reales diferentes: ');
-	write (((-b - dis) / (2 * a)):6:2, ' y ');
-	writeln (((-b + dis) / (2 * a)):6:2)
-end
+if	dis = 0
+then	writeln ('Dos raíces reales iguales: ', (-b / (2 * a)):6:2)
 else
-	if	dis = 0
-	then	writeln ('Dos raíces reales iguales: ', (-b / (2 * a)):6:2)
+begin
+	if	dis > 0
+	then
+	begin
+		dis := sqrt(dis);
+		write ('Dos raíces reales diferentes: ');
+		write (((-b - dis) / (2 * a)):6:2, ' y ');
+		writeln (((-b + dis) / (2 * a)):6:2)
+	end
 	else
 	begin
 		dis := sqrt(-dis);
@@ -33,4 +34,6 @@ else
 		write ((-b / (2 * a)):6:2, ' (+/-) i ');
 		writeln ((dis / (2 * a)):6:2)
 	end
+end
+
 end.
