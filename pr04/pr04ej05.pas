2170c4195242ecf17977ba8894f0293b041b542c
quelopelo <88778736+quelopelo@users.noreply.github.com>
2021-09-03 21:23:59 -0300

    Add files via upload

--- pr04/pr04ej05.pas
+++ pr04/pr04ej05.pas
@@ -1,7 +1,7 @@
 program pr04ej05;
 
 var
-n, i : integer;
+n, i, raiz : integer;
 
 begin
 
@@ -10,11 +10,20 @@ readln(n);
 
 write('Los divisores son: 1'); 		{1 siempre es divisor}
 
-for	i := 2 to (n div 2) do		{Se itera hasta la mitad por eficiencia}
+raiz := trunc(sqrt(n));			{Se itera hasta trunc(raiz) por eficiencia}
+
+for	i := 2 to raiz do
 	if	n mod i = 0
 	then	write(' ', i:1);
-		
-if	n > 1				{Si n = 1, el divisor 1 ya se ingresó}
+
+if	n div raiz = raiz		{Se actualiza raiz para evitar divisores repetidos}
+then	raiz := raiz - 1;
+
+for	i := raiz downto 2 do
+	if	n mod i = 0
+	then	write(' ', (n div i):1);
+
+if	n > 1 				{Si n = 1, el divisor 1 ya se ingresó}
 then	write(' ', n:1); 		{n siempre es divisor}
 
 writeln
