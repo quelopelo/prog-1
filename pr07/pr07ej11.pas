940e4b0377e93d61fd7f70156db0b482dff2c055
quelopelo <88778736+quelopelo@users.noreply.github.com>
2021-09-09 10:09:40 -0300

    Update pr07ej11.pas

--- pr07/pr07ej11.pas
+++ pr07/pr07ej11.pas
@@ -2,24 +2,19 @@ program pr07ej11;
 
 var	i, m, n : integer;
 
-function es_primo (n : integer) : boolean;
+function es_primo(n : integer) : boolean;
 {Precondición: n >= 2}
 var	i, m : integer;
 begin
 	if	n mod 2 = 0
-	then
-		if	n = 2
-		then	es_primo := true
-		else	es_primo := false
+	then	es_primo := n = 2
 	else
 	begin
 		i := 3;
 		m := trunc(sqrt(n));
 		while	(n mod i > 0) and (i <= m) do
 			i := i + 2;
-		if	i > m
-		then	es_primo := true
-		else	es_primo := false
+		es_primo := i > m
 	end
 end;
 
