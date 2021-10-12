e5c7eef7000284502bc865c243500d0f0f6feb62
quelopelo <88778736+quelopelo@users.noreply.github.com>
2021-10-12 11:54:23 -0300

    Update pr10ej09.pas

--- pr10/pr10ej09.pas
+++ pr10/pr10ej09.pas
@@ -4,8 +4,8 @@ type	raro = (gugol, nudol, brudol, cudol, zudol, budol);
 
 begin
 
-writeln (ord(gugol) : 1);		{0}
-writeln (ord(zudol) : 1);		{4}
-writeln (ord(succ(zudol)) : 1)	{5}
+writeln (ord(gugol) : 1);   {0}
+writeln (ord(zudol) : 1);   {4}
+writeln (ord(succ(zudol)) : 1)   {5}
 
 end.
